33900816bb3d77caa91fdc90e0a43d858f97f86c
Huajieyu001 <331173500@qq.com>
2026-03-19 01:17:13 +0800

    init

---
+++ UploadApp/src/main/java/top/huajieyu001/UploadApplication.java
@@ -0,0 +1,18 @@
+package top.huajieyu001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author huajieyu
+ * @Date 2026/3/19 1:16
+ * @Version 1.0
+ * @Description TODO
+ */
+@SpringBootApplication
+public class UploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadApplication.class, args);
+    }
+}
